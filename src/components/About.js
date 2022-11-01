a5d25f89f9a7b06aa71189798cc824c24dcb6872
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-31 22:02:15 -0400

    commit tree eb5f4f3a59fac9901bc0cbce84932be554d71366

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id='about'>
+      <H2>About me</H2>
+      <p>I love Jesus Christ</p>
+      <img src={image} alt='I made this' /> 
+    </div>
+  )
 }
 
 export default About;
